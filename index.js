627f8708ce31be77063981cbe30480acf320017b
Sandra Cuéllar <sandra.cllp.22@gmail.com>
2021-07-01 23:22:08 +0200

    memoria lire

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const chalk = require ("chalk");
 const { win32 } = require("path");
 const sistemaOperativo = os.platform();
 console.log('Hackeando tu sistema operativo...');
-
+let memoriaLibre;
 
 console.log(sistemaOperativo);
 if (sistemaOperativo === "win32") console.log("Tu no molas mucho");
 if (sistemaOperativo ==="darwin") console.log("Tú no molas nada. Bueno, excepto si eres Geraldine o Pol. En ese caso molas. Pero por ser tú, no por usar Mac");
 if (sistemaOperativo ==="linux") console.log("Tú molas");
+os.freemem()
+memoriaLibre = os.freemem()/1024/1024;
+console.log(`Cuidado, te quedan ${memoriaLibre} Mb de RAM libre`);
