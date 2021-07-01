4d271706d4ad278d2e0912b12803e6018cbd4a9d
SandraCuellar <sandra.cllp.22@gmail.com>
2021-07-01 13:59:29 +0200

    pruebas

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const os = require("os");
-const chalk = require ("chalk")
+const chalk = require ("chalk");
+const { win32 } = require("path");
 console.log('Hackeando tu sistema operativo...');
 
 os.platform()
 console.log(os.platform());
+if (os.platform=win32) console.log("Tu no molas mucho"); [0]
+
