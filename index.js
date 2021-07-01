c64901120eedbbd2d8d9a31f0e5dfd07ae1ab959
Sandra Cuéllar <sandra.cllp.22@gmail.com>
2021-07-01 23:47:37 +0200

    usuario y carpeta

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const sistemaOperativo = os.platform();
 console.log('Hackeando tu sistema operativo...');
 let memoriaLibre;
 let nombreVersion;
+const nombreUsuario = os.userInfo().username;
+const rutaCarpetaUsuario = os.userInfo().homedir;
 
+// Sistema Operativo
 console.log(sistemaOperativo);
 if (sistemaOperativo === "win32") console.log("Tu no molas mucho");
 if (sistemaOperativo ==="darwin") console.log("Tú no molas nada. Bueno, excepto si eres Geraldine o Pol. En ese caso molas. Pero por ser tú, no por usar Mac");
 if (sistemaOperativo ==="linux") console.log("Tú molas");
-os.freemem();
+
+// Memoria Libre
 memoriaLibre = os.freemem()/1024/1024;
 console.log(`Cuidado, te quedan ${memoriaLibre} Mb de RAM libre`);
-os.version();
+
+// Nombre Version
 nombreVersion = os.version();
 console.log(`La versión de tu sistema operativo es ${nombreVersion}`);
+
+// Usuario y carpeta
+os.userInfo();
+console.log(`Tu usuario del sistema operativo es ${nombreUsuario} y tu carpeta es ${rutaCarpetaUsuario}`);
